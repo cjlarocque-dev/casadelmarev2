a1c4aa4e83118e41c6cfc33fcd7513f124c353e1
cjlarocque-dev <cjlarocque@example.com>
2026-09-21 06:56:16 -0400

    Remove next.config redirect and add .htaccess for root redirect at server level

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/casadelmare",
   trailingSlash: false,
-  redirects: async () => {
-    return [
-      {
-        source: "/",
-        destination: "/casadelmare/",
-        basePath: false,
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
